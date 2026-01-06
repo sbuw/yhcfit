fbc5a9425307d350b9331203482cbca4b57af753
sbuw <esivolap1@gmail.com>
2026-01-06 13:52:21 +0500

    autoupdate static

--- fitapp_project/settings.py
+++ fitapp_project/settings.py
@@ -134,3 +134,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
